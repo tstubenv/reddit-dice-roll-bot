8ab1bdbc11ab404c24c0be671d01298ffa5b6866
Tyler Stubenvoll <tstubenv@nullbase.net>
2013-08-10 09:42:48 -0700

    updated source from u/oldcrow

--- diceRollBot.py
+++ diceRollBot.py
@@ -6,139 +6,131 @@ import random
 import traceback
 import sys
 
-COMMENT_TEMPLATE = "Here are your dice rolls!"
+COMMENT_TEMPLATE = "Here are your dice rolls!\n\n"
 BOT_DISCLAIMER = "^I'm ^a ^bot! ^PM ^me ^if ^something ^seems ^off."
-results = []
-values  = []
+ROLL_TEMPLATE = "for %(command)s:  \n%(total)s (rolled %(rolls)s)\n\n"
+INVALID_ROLL = "for %(command)s:  \nI'm sorry Dave, I'm afraid I can't do that.\n\n"
 user_agent = ("Random Dice Roll for r/rpg by /u/oldecrow")
-r = praw.Reddit(user_agent=user_agent)
-phrase = re.compile('\[r\d*d\d*[^0-9]{0,1}\d*\]')
-test = re.compile('[^0-9]{1,}')
-add = re.compile('\+')
-sub = re.compile('\-')
-div = re.compile('\/')
-mult = re.compile('\*')
-perc = re.compile('\%')
-mod = None
-f = open('LatestDone.conf','r')
-try: 
-	comment_id = f.read()
-	comment_id = comment_id.rstrip()
-	controlNumber = 1
-except:
-	traceback.print_exc(file=sys.stdout)
-	f.close()
-	sys.exit("Couldn't read file")
-finally:
-	f.close()
+regex = re.compile(r"""
+(?P<command>        # start command group
+\[                  # '[' - opening bracket syntax
+r                   # 'r' - rolling dice syntax
+(?P<count>\d{1,2})  # 0-99 - number of dice
+d                   # 'd' - rolling dice syntax
+(?P<sides>\d{1,5})  # 0-99999 - number of sides
+(                   # start optional modifier group
+    (?P<operator>[\-+/*])   # '-', '+', '/', '*' - operator
+    (?P<modifier>\d{1,5})   # 0-99999 - number to apply with operator
+)?                  # end optional modifier group
+\]                  # ']' - closing bracket syntax
+)                   # end command group
+""", re.VERBOSE)
+# condensed form:
+# r'(?P<command>\[r(?P<count>\d{1,2})d(?P<sides>\d{1,5})((?P<operator>[\-+/*])(?P<modifier>\d{1,5}))?\])'
 
-########################################################################
-r.login('DiceRollBot','REDACTED') ####REMOVE WHEN POSTING CODE#######
-########################################################################
 
-print "logging in..."
-#subreddit = r.get_subreddit('bottest')
-subreddit = r.get_subreddit('rpg')
-comments = subreddit.get_comments()
-for comment in subreddit.get_comments(limit=100):
-	if controlNumber == 1:
-		latestID = comment.id
-	print "...fetching..."
-	values = []
-	if phrase.search(comment.body) and comment.id != comment_id:
-		try:
-			print "rolling..."
-			print comment.id
-			#print comment.body
-			rolls = phrase.findall(comment.body)
-			for q in range(len(rolls)):
-				rolls[q] = rolls[q].strip('\[r\]')
-				rolls[q] = rolls[q].split('d')
-				if test.search(rolls[q][1]) != None:
-					if add.search(rolls[q][1]) != None:
-						temp = [rolls[q][0]]
-						rolls[q]= temp+rolls[q][1].split('+')
-						mod = '+'
-					elif mult.search(rolls[q][1]) != None:
-						temp = [rolls[q][0]]
-						rolls[q]= temp+rolls[q][1].split('*')
-						mod = '*'
-					elif perc.search(rolls[q][1]) != None:
-						temp = [rolls[q][0]]
-						rolls[q]= temp+rolls[q][1].split('%')
-						mod = '%'
-					elif sub.search(rolls[q][1]) != None:
-						temp = [rolls[q][0]]
-						rolls[q]= temp+rolls[q][1].split('-')
-						mod = '-'
-					elif div.search(rolls[q][1]) != None:
-						temp = [rolls[q][0]]
-						rolls[q]= temp+rolls[q][1].split('/')
-						mod = '/'
-				if int(rolls[q][0])<=20:
-					for i in range(int(rolls[q][0])):
-						number = random.randint(1,int(rolls[q][1]))
-						if mod != None:
-							if mod == '+':
-								number += int(rolls[q][2])
-							elif mod == '*':
-								number *= int(rolls[q][2])
-							elif mod == '%':
-								number = number/float(rolls[q][1])
-								number = number * 100
-								number = str(number)+'%'
-							elif mod == '-':
-								number -= int(rolls[q][2])
-							elif mod == '/':
-								number /= int(rolls[q][2])
-						results.append(number)
-					values.append(results)
-					results = []
-					mod == None
-				else:
-					values.append('I can\'t do more than 20 dice at a time :\ ')
-					mod == None
-			if len(values) <= 15:
-				for p in range(len(values)):
-					try:
-						if p == 0:
-							string = "for [r%sd%s]:  \n%s" % (rolls[p][0],rolls[p][1],str(values[p]).strip('\[\]'))
-						else:
-							string = string+"  \n"+"for [r%sd%s]:  \n%s" % (rolls[p][0],rolls[p][1],str(values[p]).strip('\[\]'))
-					except:
-						break
-			else:
-				p = 0
-				while p <= 15:
-					try:
-						if p == 0:
-							string = "for [r%sd%s]:  \n%s" % (rolls[p][0],rolls[p][1],str(values[p]).strip('\[\]'))
-						else:
-							string = string+"  \n"+"for [r%sd%s]:  \n%s" % (rolls[p][0],rolls[p][1],str(values[p]).strip('\[\]'))
-						if p==15:
-							string = string+"  \n"+"maximum of 15 roll commands per comment reached."
-						p+=1
-					except:
-						break
-			print "replying..."
-			comment.reply(COMMENT_TEMPLATE+"  \n"+string+"  \n"+BOT_DISCLAIMER)
-		except:
-			print "ERROR!:\n"+comment.body
-			traceback.print_exc(file=sys.stdout)
-	#else:
-		#print "nothing"
-	elif comment.id == comment_id:
-		f = open('LatestDone.conf', 'w')
-		try:
-			f.write(latestID)
-			print "configuring..."
-			controlNumber += 42
-		except:
-			traceback.print_exc(file=sys.stdout)
-			f.close()
-			sys.exit("Couldn't write file")
-		finally:
-			f.close()
-			sys.exit('done!')
-	time.sleep(1)
-	mod = None
+def roll_dice(count, sides, operator=None, modifier=None, **kwargs):
+    """Roll any number of dice with optional modifiers."""
+    results = [] # keep track of each roll
+    for die in count:
+        results.append(random.randint(1, sides))
+    total = sum(results)
+    if modifier is None or operator is None:
+        return (total, results)
+    # apply modifier
+    if operator == '+':
+        total += modifier
+    elif operator == '-':
+        total -= modifier
+    elif operator == '/':
+        total /= modifier
+    elif operator == '*':
+        total *= modifier
+    return (total, results)
+
+
+def main():
+    # try to load config
+    f = open('LatestDone.conf','r')
+    try: 
+        comment_id = f.read()
+        comment_id = comment_id.rstrip()
+        controlNumber = 1
+    except:
+        traceback.print_exc(file=sys.stdout)
+        f.close()
+        sys.exit("Couldn't read file")
+    finally:
+        f.close()
+    
+    r = praw.Reddit(user_agent=user_agent)
+    ########################################################################
+    r.login('DiceRollBot','REDACTED') ####REMOVE WHEN POSTING CODE#######
+    ########################################################################
+    
+    print "logging in..."
+    #subreddit = r.get_subreddit('bottest')
+    subreddit = r.get_subreddit('rpg')
+    comments = subreddit.get_comments()
+    
+    for comment in subreddit.get_comments(limit=100):
+        if controlNumber == 1:
+            latestID = comment.id
+        print "...fetching..."
+        
+        if comment.id == comment_id:
+            f = open('LatestDone.conf', 'w')
+            try:
+                f.write(latestID)
+                print "configuring..."
+                controlNumber += 42
+            except:
+                traceback.print_exc(file=sys.stdout)
+                f.close()
+                sys.exit("Couldn't write file")
+            finally:
+                f.close()
+                sys.exit('done!')
+            return # just in case sys.exit() didn't work...
+        
+        # build the comment string
+        cumulative_roll_string = ''
+        for roll_count, regex_match in enumerate(regex.finditer(comment.body)):
+            try:
+                # stop after 15 rolls
+                if roll_count >= 15: # roll_count starts from 0
+                    cumulative_roll_string += "Maximum of 15 roll commands per comment reached.\n\n"
+                    break
+                print "rolling..."
+                print comment.id
+                #print comment.body
+                
+                # parse command into a user-friendly dictionary
+                command_dict = regex_match.groupdict()
+                
+                # do some pre-checking for invalid parameters
+                if command_dict['sides'] == 0:
+                    roll_string = INVALID_ROLL % command_dict
+                elif command_dict['operator'] == '/' and command_dict['modifier'] == 0:
+                    roll_string = INVALID_ROLL % command_dict
+                else:
+                    total, individual_rolls = roll_dice(**command_dict)
+                
+                    # fill roll string template with data
+                    roll_string = ROLL_TEMPLATE % {
+                        'command': command_dict['command'],
+                        'total': total,
+                        'rolls': ', '.join(individual_rolls)
+                    }
+                
+                # add output string to other rolls
+                cumulative_roll_string += roll_string
+            except:
+                print "ERROR!:\n"+comment.body
+                traceback.print_exc(file=sys.stdout)
+        print "replying..."
+        comment.reply(COMMENT_TEMPLATE + cumulative_roll_string + BOT_DISCLAIMER)
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()
